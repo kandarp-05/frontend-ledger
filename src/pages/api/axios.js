8f1c5ef3fbf3478d2c8fdba6f338611986912f0f
Mobcoder ID-100 <mobcoderid-100@Mobcoders-Mac-mini.local>
2026-07-17 17:20:46 +0530

    deploying

--- src/pages/api/axios.js
+++ src/pages/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-    baseURL: ["http://localhost:8000/api","https://backend-ledger-1twm.onrender.com"],
+    baseURL: ["http://localhost:8000/api","https://backend-ledger-1twm.onrender.com/api"],
     withCredentials: true,
 });
 
